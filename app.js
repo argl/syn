8a8c16370fc722da94ae06783463465057536489
argl <andi@null2.net>
2015-10-18 00:17:46 +0200

    cors woes

--- app.js
+++ app.js
@@ -25,7 +25,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+  // setHeaders: function(res, path) {
+  //   res.setHeader("Access-Control-Allow-Origin", "*");
+  // }
+}));
 
 app.use(function(req, res, next) {
   req.io = app.io
